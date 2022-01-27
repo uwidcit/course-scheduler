ea23cb091699f2dbed6503316ec0568eb687d1f8
Jeremiah Strong <jeremiahstrong321@gmail.com>
2022-01-26 22:28:09 -0400

    added template with gitpod config

---
+++ .gitpod.Dockerfile
@@ -0,0 +1,12 @@
+FROM gitpod/workspace-full-vnc
+
+# Install Cypress dependencies.
+RUN sudo apt-get update  && sudo DEBIAN_FRONTEND=noninteractive apt-get install -y    libgtk2.0-0    libgtk-3-0    libnotify-dev    libgconf-2-4    libnss3    libxss1    libasound2    libxtst6    xauth    xvfb  && sudo rm -rf /var/lib/apt/lists/*
+
+
+RUN npm i -g firebase-tools
+RUN npm i -g @angular/cli
+
+#set puppeteer chrome as chrome
+ENV CHROME_BIN=/workspace/starterkit/node_modules/puppeteer/.local-chromium/linux-938248/chrome-linux/chrome
+#RUN gp env --export CHROME_BIN=/workspace/starterkit/node_modules/puppeteer/.local-chromium/linux-938248/chrome-linux/chrome
---
+++ protractor.conf.js
@@ -0,0 +1,30 @@
+const { SpecReporter } = require('jasmine-spec-reporter');
+
+exports.config = {
+  allScriptsTimeout: 20000,
+  specs: [
+    './e2e/**/*.e2e-spec.ts'
+  ],
+  capabilities: {
+    'browserName': 'chrome',
+    chromeOptions: {
+      binary: process.env.CHROME_BIN,
+      args: ['--headless', '--disable-gpu', '--window-size=800,600', '--no-sandbox']
+    }
+  },
+  directConnect: true,
+  // SELENIUM_PROMISE_MANAGER: false,
+  baseUrl: 'http://localhost:4200/',
+  framework: 'jasmine',
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 20000,
+    print: function() {}
+  },
+  onPrepare() {
+    require('ts-node').register({
+      project: 'e2e/tsconfig.e2e.json'
+    });
+    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+  }
+};
